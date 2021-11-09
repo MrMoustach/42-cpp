f16297b8ef47a5f2040a73d3d74f1ee0942f2334
mrmoustach <bossissamos@gmail.com>
2021-11-09 11:31:27 +0100

    re did it another time

--- cpp-00/ex01/main.cpp
+++ cpp-00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 19:24:03 by iharchi           #+#    #+#             */
-/*   Updated: 2021/11/09 10:53:04 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/11/09 11:27:10 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int	main(void)
 	Phonebook	phonebook = Phonebook();
 	char	is;
 	int	i;
+	char	flag;
 	std::string	prompts[5] = {"First name", "Last name", "Nickname", "Phone Number", "Darkest secret"};
 
-	std::cout << "Welcome to your phonebook : \n";
-	while (1)
+	std::cout << "Welcome to your phonebook : \n> ";
+	while (std::getline(std::cin, command))
 	{
-		std::cout << "> ";
-		std::cin >> command;
+		// std::cin >> command;
+		if (command == "")
+			continue ;
 		if (command == "EXIT")
-		{
 			exit (0);
-		}
 		else if (command == "ADD")
 		{
 			std::cout << "--Fill data--\n";
@@ -47,18 +47,18 @@ int	main(void)
 			phonebook.prompt_search();
 			std::cout << "Please choose an index to see : ";
 			std::cin >> is;
+			flag = 0;
+			flag = std::cin.get();
 			i = atoi(&is);
-			if (isdigit(is) && i < phonebook.get_current_count() && i >= 0)
+			if (isdigit(is) && i < phonebook.get_current_count() && i >= 0 && flag == '\n')
 				phonebook.print_contact(i);
 			else
-			{
 				std::cout << "Please choose an index between 0 and lower than "<< phonebook.get_current_count() << "\n";
-				std::cin.clear();
-			}
 			i = 0;
 		}
 		else
 			std::cout << "Please choose one of the possible commands (ADD, SEARCH, EXIT) \n";
+		std::cout << "> ";
 	}
-	return (0);
+	return (1);
 }

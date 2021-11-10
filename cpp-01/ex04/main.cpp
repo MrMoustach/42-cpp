595965df9092e4eb1cb6a648133041ca21a58334
mrmoustach <bossissamos@gmail.com>
2021-11-10 13:08:35 +0100

    finished ex04

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 18:51:22 by iharchi           #+#    #+#             */
-/*   Updated: 2021/11/09 19:09:17 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/11/10 13:06:06 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,43 @@ int main(int ac, char** av)
 	std::string	line;
 	std::size_t	found;
 
-	(void) ac;
+	if (ac != 4)
+	{
+		std::cerr << "Please provide 3 argument as ./replace [file] [string to replace] [replacement]";
+	}
 	file.open(av[1]);
-	outfile.open(av[1] + ".replace");
+	
+	std::string s = ".replace";
+	s = av[1] + s;
+	outfile.open(s);
 	if (!file)
 		std::cerr << "File could not be opened\n";
 	else
 	{
 		while (std::getline(file, line))
 		{
-			found = line.find("ha");
-			if (found != std::string::npos)
+			found = 0;
+			std::size_t i = 0;
+			while (1)
 			{
-				std::cout << found << std::endl;
+				found = line.find(av[2], found);
+				if (found == std::string::npos)
+				{
+					outfile << &line[i];
+					break ;
+				}
+				for (; i < found; i++)
+				{
+					if (i >= line.length())
+						break ;
+					outfile << line[i];
+				}
+				if (found < line.length())
+					outfile << av[3];
+				found += strlen(av[2]);
+				i = found;
 			}
+			outfile << std::endl;
 		}
 		file.close();
 		outfile.close();

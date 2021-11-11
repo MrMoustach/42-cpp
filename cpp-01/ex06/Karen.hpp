9d4fd9dd740beae7591614bd05608a25c12010fe
mrmoustach <bossissamos@gmail.com>
2021-11-11 03:07:37 +0100

    finished ex06

--- cpp-01/ex06/Karen.hpp
+++ cpp-01/ex06/Karen.hpp
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Karen.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/11 01:56:37 by iharchi           #+#    #+#             */
-/*   Updated: 2021/11/11 03:02:18 by iharchi          ###   ########.fr       */
+/*   Created: 2021/11/11 01:37:37 by iharchi           #+#    #+#             */
+/*   Updated: 2021/11/11 03:00:36 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Karen.hpp"
+#include <iostream>
+#include <string>
 
-int	main()
+class Karen
 {
-	Karen karen = Karen();
-	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	for (int i = 0; i < 4; i++)
-	{
-		std::cout << levels[i] << " : ";
-		karen.complain(levels[i]);
-		
-	}
-	return (0);
-}
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		typedef void (Karen::*fnptr)();
+		fnptr action[4];
+		Karen();
+		~Karen();
+		void complain(std::string level);
+};
+int index_of(std::string level, std::string *levels);
--- cpp-01/ex06/main.cpp
+++ cpp-01/ex06/main.cpp
@@ -6,21 +6,39 @@
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 01:56:37 by iharchi           #+#    #+#             */
-/*   Updated: 2021/11/11 03:02:18 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/11/11 03:06:55 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
-int	main()
+int	main(int ac, char** av)
 {
 	Karen karen = Karen();
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	for (int i = 0; i < 4; i++)
+	(void) ac;
+	if (ac < 2)
 	{
-		std::cout << levels[i] << " : ";
-		karen.complain(levels[i]);
-		
+		std::cout << "Please provide a level of Karen" << std::endl;
+		return (1);
+	}
+	switch (index_of(av[1], levels)) 
+	{
+		case 0:
+			std::cout << "[ DEBUG ]" << std::endl;
+			karen.complain("DEBUG");
+		case 1:
+			std::cout << "[ INFO ]" << std::endl;
+			karen.complain("INFO");
+		case 2:
+			std::cout << "[ WARNING ]" << std::endl;
+			karen.complain("WARNING");
+		case 3:
+			std::cout << "[ ERROR ]" << std::endl;
+			karen.complain("ERROR");
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 	return (0);
 }

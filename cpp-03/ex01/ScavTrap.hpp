fe28c8e737fbe3faa6ce50086b130fb68ee6e12e
mrmoustach <bossissamos@gmail.com>
2021-11-21 02:50:09 +0100

    prolly finished ex01

--- cpp-03/ex01/ScavTrap.hpp
+++ cpp-03/ex01/ScavTrap.hpp
@@ -1,42 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/20 22:29:01 by iharchi           #+#    #+#             */
-/*   Updated: 2021/11/21 02:41:11 by iharchi          ###   ########.fr       */
+/*   Created: 2021/11/21 02:25:35 by iharchi           #+#    #+#             */
+/*   Updated: 2021/11/21 02:47:11 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAP_TRAP
-# define CLAP_TRAP
-#include <iostream>
-#include <string>
+#include "ClapTrap.hpp"
 
-class ClapTrap{
-	protected:
-		std::string _name;
-		int	_hp;
-		int	_energy;
-		int	_ad;
+class ScavTrap : public ClapTrap
+{
 	public:
-		ClapTrap();
-		ClapTrap(std::string name);
-		ClapTrap(const ClapTrap & ct);
-		~ClapTrap();
-		ClapTrap& operator=(ClapTrap ct);
-		std::string& getName();
-		int	getHp() const;
-		int	getEnergy() const;
-		int	getAd() const;
-		void setName(std::string name);
-		void setHp(int hp);
-		void setEnergy(int energy);
-		void setAd(int ad);
+		ScavTrap(std::string name);
+		~ScavTrap();
 		void attack(std::string & target);
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
+		void guardGate();
 };
-#endif
--- cpp-03/ex01/main.cpp
+++ cpp-03/ex01/main.cpp
@@ -6,21 +6,22 @@
 /*   By: iharchi <iharchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 22:56:27 by iharchi           #+#    #+#             */
-/*   Updated: 2021/11/20 23:03:41 by iharchi          ###   ########.fr       */
+/*   Updated: 2021/11/21 02:49:31 by iharchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main(void)
 {
 	ClapTrap clapTrap = ClapTrap("V1");
-	ClapTrap clapTrap2 = ClapTrap("V2");
+	ScavTrap scavTrap = ScavTrap("V2");
 
-	clapTrap.attack(clapTrap2.getName());
-	clapTrap2.takeDamage(10);
-	clapTrap2.attack(clapTrap.getName());
+	clapTrap.attack(scavTrap.getName());
+	scavTrap.takeDamage(10);
+	scavTrap.attack(clapTrap.getName());
 	clapTrap.takeDamage(10);
 	clapTrap.beRepaired(10);
-	clapTrap2.beRepaired(5);
+	scavTrap.beRepaired(5);
+	scavTrap.guardGate();
 }
